e068e4e40789c6756ee65a10ea2f7e374371a745
Niclas <147914782+GarNicWahr@users.noreply.github.com>
2024-03-26 12:16:02 +0100

    Character Importiert

--- GameJam/Assets/Vorlesung/01-Movement/Scripts/Refactoring/ball.cs
+++ GameJam/Assets/Vorlesung/01-Movement/Scripts/Refactoring/ball.cs
@@ -1,59 +1,67 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
-public class ball : MonoBehaviour {
-    
+public class Ball : MonoBehaviour
+{
+
     // Bewegungs-Geschwindigkeit
-    public float speed = 0;
+    public float MovementSpeed = 0;
 
     // Sprung Stärke
-    public float jmpStr = 50;
+    public float JumpStrength = 50;
 
     // Rigidbody of the player.
-    private Rigidbody rb;
+    private Rigidbody _rigidBody;
 
     // Bewegung entlang der X- und Y-Achse.
-    private float movX;
-    private float movY;
-    
+    private float _movementX;
+    private float _movementY;
+
 
     // Start is called before the first frame update
-    void Start() {
-        rb = GetComponent<Rigidbody>();
+    void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update() {
-        movX = Input.GetAxis("Horizontal");
-        movY = Input.GetAxis("Vertical");
+    void Update()
+    {
+        _movementX = Input.GetAxis("Horizontal");
+        _movementY = Input.GetAxis("Vertical");
 
-        if(Input.GetKeyDown(KeyCode.Space) && GroundCheck()) {
-            Jumping();
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded())
+        {
+            Jump();
         }
     }
 
-    void FixedUpdate() {
-        Movement();
+    void FixedUpdate()
+    {
+        Move();
     }
 
-    private void Movement() {
-        Vector3 Movement = new Vector3(movX, 0.0f, movY);
-        rb.AddForce(Movement * speed);
+    private void Move()
+    {
+        Vector3 movement = new Vector3(_movementX, 0.0f, _movementY);
+        _rigidBody.AddForce(movement * MovementSpeed);
     }
 
-    private void Jumping() {
-        rb.AddForce(Vector3.up * jmpStr, ForceMode.Impulse);
+    private void Jump()
+    {
+        _rigidBody.AddForce(Vector3.up * JumpStrength, ForceMode.Impulse);
     }
 
-    private bool GroundCheck() {
+    private bool isGrounded()
+    {
         RaycastHit hit;
-        if(Physics.Raycast(transform.position, -Vector3.up, out hit, 0.6f)) {
+        if (Physics.Raycast(transform.position, -Vector3.up, out hit, 0.6f))
+        {
             print(hit.collider.name);
             return true;
         }
-        else {
+        else
+        {
             return false;
         }
     }
