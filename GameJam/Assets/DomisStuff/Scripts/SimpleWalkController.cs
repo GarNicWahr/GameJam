d72dccec042d4ae286b5499c08286521298320f1
MafackerJones <lust20035@hs-ansbach.de>
2024-04-22 16:17:14 +0200

    Changes to Domis Scene
    - Added Running to Movement Script
    - Added new Animations
    + (Walking backwards, Running Backwards for next Session)
    + Running Forwards

--- GameJam/Assets/DomisStuff/Scripts/SimpleWalkController.cs
+++ GameJam/Assets/DomisStuff/Scripts/SimpleWalkController.cs
@@ -6,22 +6,31 @@ using UnityEngine;
 public class SimpleWalkController : MonoBehaviour
 {
     // Defines how fast the GO can move
-    public float movementSpeed = 10;
+    public float movementSpeed = 3;
 
     // Defines how fast the GO can turn
     public float turnSpeed = 90;
 
-    //Define the Animator Component
+    public float runningSpeed = 20;
+
+    // Define the Animator Component
     private Animator _animator;
 
+    // Define the variable names for Animator Hashes
     private int isWalkingParameterHash;
     private int directionParameterHash;
+    private int isRunningParameterHash;
+
+    // Definde if LeftShift is pressed or not
+    private bool isRunning;
 
     private void Start()
     {
+        // Define the Animator Component and its variables to hashes
         _animator = GetComponent<Animator>();
         isWalkingParameterHash = Animator.StringToHash("isWalking");
         directionParameterHash = Animator.StringToHash("direction");
+        isRunningParameterHash = Animator.StringToHash("isRunning");
     }
     // Update is called once per frame
     void Update()
@@ -30,6 +39,18 @@ public class SimpleWalkController : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         float verticalInputRaw = Input.GetAxisRaw("Vertical");
+
+        // Are we running
+        /// Yes
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            movementSpeed = runningSpeed;
+        }
+        /// No
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            movementSpeed = 3;
+        }
 
         // Translate along z-axis
         transform.Translate(Vector3.forward * verticalInput * movementSpeed * Time.deltaTime);
@@ -40,11 +61,12 @@ public class SimpleWalkController : MonoBehaviour
             // calculate the angle around y-axis
             float turnAngle = turnSpeed * horizontalInput * Input.GetAxisRaw("Vertical") * Time.deltaTime;
             transform.Rotate(Vector3.up, turnAngle);
-            _animator.SetBool("isWalking", true);
         }
+
 
         _animator.SetBool(isWalkingParameterHash, verticalInputRaw != 0);
         _animator.SetFloat(directionParameterHash, verticalInputRaw);
-        
+        _animator.SetBool(isRunningParameterHash, Input.GetKey(KeyCode.LeftShift));
     }
+
 }
